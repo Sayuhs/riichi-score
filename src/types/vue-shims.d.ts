98ad9b5353ccb62e7f0fc13947dca36f0522cd04
Shao Yuhe <2898549292@qq.com>
2026-09-23 12:36:07 +0800

    修复已知问题和优化ui

--- src/types/vue-shims.d.ts
+++ src/types/vue-shims.d.ts
@@ -2,7 +2,7 @@
  * `.vue` 单文件组件的类型声明。
  *
  * 由 Vite + @vitejs/plugin-vue 在构建时处理，TypeScript 需要知道
- * 这些模块存在，否则 `import X from "./X.vue"` 会报找不到模块。
+ * 这些模块存在，否则 `import X from "./X.vue"` 会报找不到模块
  */
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
